0c9a08736f8629e6dc2ba75001fd8846f320d6c8
Patrick <Patrick@gdi2290.com>
2013-01-29 14:43:40 -0800

    add photo in about

--- Gemfile
+++ Gemfile
@@ -14,8 +14,6 @@ group :production do
  gem 'pg'
 end
 
-# Gems used only for assets and not required
-# in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2'
   gem 'coffee-rails', '~> 3.2'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,9 @@ class UserMailer < ActionMailer::Base
 
   def contact(options)
     @options = options
-    mail(to: "contact@maderapawn.com", subject: "#{@options[:name]} stuff", from: "no-reply@maderapawn.com").deliver
+
+    mail(to: "suzy@maderapawn.com", subject: "#{@options[:name]} stuff", from: "no-reply@maderapawn.com").deliver
+
+    # mail(to: "gary@maderapawn.com", subject: "#{@options[:name]} stuff", from: "no-reply@maderapawn.com").deliver
   end
 end
