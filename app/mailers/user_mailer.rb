d93c2bfe31c5ae459f7f2d41836e36670f586f19
Patrick <Patrick@gdi2290.com>
2013-01-31 13:05:12 -0800

    fix forms

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def contact(options)
     @options = options
 
-    mail(to: "suzy@maderapawn.com", subject: "#{@options[:name]} stuff", from: "no-reply@maderapawn.com").deliver
+    mail(to: "suzy@maderapawn.com", subject: "#{@options[:subject]} from #{@options[:name]}", from: "no-reply@maderapawn.com").deliver
 
     # mail(to: "gary@maderapawn.com", subject: "#{@options[:name]} stuff", from: "no-reply@maderapawn.com").deliver
   end
