1d2ecbb77d0c2f30caa6d902760d928ac87cff22
Patrick <Patrick@gdi2290.com>
2013-02-04 12:33:05 -0800

    add faq and licenses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,6 @@ class HomeController < ApplicationController
 
   def send_contact
     UserMailer.contact(params)
-    redirect_to "/contact", notice: "Email has been sent! Thanks "
+    redirect_to "/contact", notice: "Email has been sent! Thanks!"
   end
 end
