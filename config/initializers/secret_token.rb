557fbf984428f51d8349d3996c9e4ee51484da25
gdi2290 <github@gdi2290.com>
2014-02-17 12:47:17 -0800

    delete init files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Maderapawn::Application.config.secret_token = '63249d409a0f72a7e6a421dab3005775999f20bca1e2924930fa7c83edb81dd8e0179b9389c6bed5b028c27ce0b67bfd8159b63857e7a730485589383f83a3f1'
